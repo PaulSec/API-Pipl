04d48f839dce9e91321695a5ce3d1677e01c00f7
Paul AMAR <aos.paul@gmail.com>
2014-09-29 21:29:20 +0100

    added usage example

--- piplAPI_example.py
+++ piplAPI_example.py
@@ -1,3 +1,9 @@
 from piplAPI import PiplAPI
+
+# verbose mode
+res = PiplAPI({'verbose': True}).get_info('Steve Wozniak')
+print res  # retrieves the results
+
+# non verbose mode
 res = PiplAPI().get_info('Steve Wozniak')
 print res  # retrieves the results
